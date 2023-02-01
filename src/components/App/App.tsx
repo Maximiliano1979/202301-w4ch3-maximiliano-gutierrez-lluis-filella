afc91c5510060327271e43797815f4984bb10950
lfilella97 <lfilella97@gmail.com>
2023-02-01 21:28:45 +0100

    Add component Info and it own styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
+import Info from "../Info/Info";
 import "./App.css";
 
 const App = (): JSX.Element => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Info />
+    </div>
+  );
 };
 
 export default App;
