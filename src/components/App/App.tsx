b646b83e87dd5e7aaec244a199d03a02aaa38c2f
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-01 21:29:57 +0100

    Add Display component and component roadmap

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
+import Display from "../Display/Display";
 import "./App.css";
 
 const App = (): JSX.Element => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Display />
+    </div>
+  );
 };
 
 export default App;
