b7856d0f43258e657bfcdc9a20c9e70b1dc2c5de
lfilella97 <lfilella97@gmail.com>
2023-02-01 22:28:27 +0100

    Add Keyboard Component

---
+++ src/components/Keyboard/Keyboard.tsx
@@ -0,0 +1,11 @@
+import "./Keyboard.css";
+
+const Keyboard = (): JSX.Element => {
+  return (
+    <div className="keyboard-container">
+      <ol className="keyboard"></ol>
+    </div>
+  );
+};
+
+export default Keyboard;
