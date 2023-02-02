b7492abff47754de0c5fefdd877592277b1db733
lfilella97 <lfilella97@gmail.com>
2023-02-02 08:10:48 +0100

    Add Key Component and it own style

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,9 +1,17 @@
+import Key from "../Key/Key";
 import "./Keyboard.css";
 
 const Keyboard = (): JSX.Element => {
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "delete"];
   return (
     <div className="keyboard-container">
-      <ol className="keyboard"></ol>
+      <ol className="keyboard">
+        {numbers.map((number) => (
+          <li>
+            <Key number={number} />
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
